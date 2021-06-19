2e1f6cb054c55bdd4076fc71a5a415d157f8baa8
“Lilia <lilia.mehamli@etu.usthb.dz>
2021-06-19 21:52:36 +0100

    download and save face images to the corresponding mbti personality folder

--- main.py
+++ main.py
@@ -2,13 +2,32 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
 import os
+import requests
 
-absolutePATH = "database/"
+def DownloadImage(dirname, url):
+    # Change directory to corresponding mbti personality folder
+    os.chdir(dirname)
+    # We can split the file based upon / and extract the last split within the python list below:
+    file_name = url.split('/')[-1]
+    print(f"This is the file name: {file_name}")
+    # let's send a request to the image URL:
+    r = requests.get(url, stream=True)
+    # We can check that the status code is 200 before doing anything else:
+    if r.status_code == 200:
+        # This command below will allow us to write the data to a file as binary:
+        with open(file_name, 'wb') as f:
+            for chunk in r:
+                f.write(chunk)
+    else:
+        # We will write all of the images back to the broken_images list:
+        broken_images.append(url)
+    # Return to absolute path
+    os.chdir('../')
 
 def CreateFolder(dirname):
     # Create target Directory if don't exist
-    if not os.path.exists(absolutePATH + dirname):
-        os.mkdir(absolutePATH + dirname)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
         print("Directory " , dirname ,  " Created ")
     else:    
         print("Directory " , dirname ,  " already exists")
@@ -29,47 +48,36 @@ def TraverseDOM(driver, PDB_url):
 
     return page_source 
 
-def ExtractData(page_source, profiles):
+def ExtractData(page_source):
     soup = BeautifulSoup(page_source, 'lxml')
     for card_profile in soup.findAll('div', attrs={'id': re.compile('card-profile-\d+')}):
-        # Initialize array
-        profile = []
-
         # Get MBTI personality type
         personality_class = card_profile.find('div', class_='card-container-personality')
         mbti = personality_class.find(text=True)
-        profile.append(mbti)
         CreateFolder(mbti)
 
         # Get MBTI personality type
         id = re.search('(?<=profile\-).*', card_profile.get('id'))[0] # get current element ID
         image_url = "https://www.personality-database.com/profile_images/"+ id +".png"
-        profile.append(image_url)
-
-        # Add profile 
-        profiles.append(profile)
-
-    return profiles
-
+        DownloadImage(mbti, image_url)
 
 def main():
     PDB_url = 'https://www.personality-database.com/trending'
-    profiles = []
     driver_location = '/usr/bin/chromedriver'
     binary_location = '/usr/bin/google-chrome'
-    
+
+    # Change directory to database/
+    os.chdir('database/')
     # Configuring Selenium webdriver
     driver = ConfigSelenium(driver_location, binary_location)
     # Traversing through the DOM of the PDB webpage
     page_source = TraverseDOM(driver, PDB_url)
     # Extracting the data with bs4
-    profiles = ExtractData(page_source, profiles)
+    ExtractData(page_source)
     # Closing browser
     driver.close()
-    # Display data
-    print(profiles)
-    
     
 
 if __name__ == "__main__":
+    
     main()
